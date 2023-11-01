6cab7f6b10a37d85ee45fe93089db622db841950
EhabBeshay <ehabbeshay1990@gmail.com>
2023-11-01 19:22:49 -0400

    This commit has the basics of solar system

---
+++ src/test/java/SolarSystem/Main.java
@@ -0,0 +1,13 @@
+package SolarSystem;
+
+public class Main {
+    public static void main(String[] args) {
+
+        SolarSystemBasics solarsys=new SolarSystemBasics();
+        solarsys.Sun="1";
+        solarsys.Moon="2";
+        solarsys.Planets="2";
+        solarsys.Stars="10";
+
+    }
+}
